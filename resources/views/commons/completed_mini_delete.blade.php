3b22a96561870e487dcd6b18f01a34d175af232e
Noboru.kamita <noboru.kamita@gamil.com>
2020-01-22 04:53:19 +0000

    change childwindows close time to 2sec

--- resources/views/commons/completed_mini_delete.blade.php
+++ resources/views/commons/completed_mini_delete.blade.php
@@ -33,7 +33,7 @@ $user_name=$user -> name;
             $('#mdOverlay,#mdWindow',parent.document).remove();
         }
         */
-        setTimeout(func1, 1000);
+        setTimeout(func1, 2000);
         //setTimeout(func2, 2000);
     });
 -->
